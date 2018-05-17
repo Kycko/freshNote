c645723ca268bb46a3785dd4ff61cd982eb27257
Anthony Samartsev <ant.samarcev@gmail.com>
2018-05-17 09:05:08 +0300

    upper-left icon (for main menu) clickable

--- app/src/main/java/com/github/kycko/freshnote/MainActivity.java
+++ app/src/main/java/com/github/kycko/freshnote/MainActivity.java
@@ -1,13 +1,30 @@
 package com.github.kycko.freshnote;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout mDrawerLayout = findViewById(R.id.DrawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
